cdac5ec230e1d14fe675545a400e51b581ba3796
nldanne <nldanne97@gmail.com>
2022-02-10 19:01:22 +0100

    CodeWars, easy, array manipulation

---
+++ categorizeNewMember.js
@@ -0,0 +1,45 @@
+/*
+CodeWars - Categorize New Members
+
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. 
+They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. 
+In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input:
+Input will consist of a list of pairs. Each pair contains information for a single potential member. 
+Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output:
+Output will consist of a list of string values (in Haskell: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+Example
+
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+ű
+*/
+
+function openOrSenior(data){
+  let categorized = [];
+  
+  for (let i = 0; i < data.length; i++) {
+    let category = '';
+    if (data[i][0] >= 55) {
+      if (data[i][1] > 7) {
+        category = "Senior";
+      } else {
+        category = 'Open';
+      }
+    } else {
+      category = 'Open';
+    }
+    categorized.push(category);
+  }
+  
+  return categorized; 
+}
+
+let array = [[45, 12],[55,21],[19, -2],[104, 20]];
+
+console.log(openOrSenior(array));
